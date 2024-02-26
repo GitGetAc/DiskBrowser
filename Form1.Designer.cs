293a7cd8f33528a1054ab5f0e63e43cb17892fa3
zoltan.ladanyi_micros@gmx.com <zoltan.ladanyi_micros@gmx.com>
2024-02-26 13:41:12 +0100

    Commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             treeView1 = new TreeView();
             listView1 = new ListView();
-            splitter1 = new Splitter();
             columnHeader1 = new ColumnHeader();
+            splitter1 = new Splitter();
             SuspendLayout();
             // 
             // treeView1
@@ -39,32 +39,34 @@
             treeView1.Dock = DockStyle.Left;
             treeView1.Location = new Point(0, 0);
             treeView1.Name = "treeView1";
-            treeView1.Size = new Size(251, 450);
+            treeView1.Size = new Size(190, 450);
             treeView1.TabIndex = 0;
+            treeView1.BeforeExpand += treeView1_BeforeExpand;
+            treeView1.AfterSelect += treeView1_AfterSelect;
             // 
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1 });
             listView1.Dock = DockStyle.Fill;
-            listView1.Location = new Point(251, 0);
+            listView1.Location = new Point(190, 0);
             listView1.Name = "listView1";
-            listView1.Size = new Size(549, 450);
+            listView1.Size = new Size(610, 450);
             listView1.TabIndex = 1;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
-            // 
-            // splitter1
-            // 
-            splitter1.Location = new Point(251, 0);
-            splitter1.Name = "splitter1";
-            splitter1.Size = new Size(3, 450);
-            splitter1.TabIndex = 2;
-            splitter1.TabStop = false;
             // 
             // columnHeader1
             // 
             columnHeader1.Text = "Filename";
             columnHeader1.Width = 200;
+            // 
+            // splitter1
+            // 
+            splitter1.Location = new Point(190, 0);
+            splitter1.Name = "splitter1";
+            splitter1.Size = new Size(8, 450);
+            splitter1.TabIndex = 2;
+            splitter1.TabStop = false;
             // 
             // Form1
             // 
@@ -76,6 +78,7 @@
             Controls.Add(treeView1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,106 @@ namespace DiskBrowser
         {
             InitializeComponent();
         }
+
+        public void ShowDrives()
+        {
+            //Begin 
+            treeView1.BeginUpdate();
+
+            //Array with Drivenames
+            string[] drives = Directory.GetLogicalDrives();
+
+            //Fill out nodes with drivenames
+            foreach (var drive in drives)
+            {
+                TreeNode treeNode = new TreeNode(drive);
+                treeView1.Nodes.Add(treeNode);
+
+                //Calling AddDirs in ShowDrives()
+                AddDirs(treeNode);
+            }
+
+
+
+            //End
+            treeView1.EndUpdate();
+        }
+
+        public void ShowFileNames()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(treeView1.SelectedNode.FullPath);
+            FileInfo[] filesArray = { };
+            ListViewItem listViewItem;
+
+            listView1.Items.Clear();
+
+            if (directoryInfo.Exists)
+            {
+                filesArray = directoryInfo.GetFiles(); //Todo 1.: Exception handling
+            }
+
+            listView1.BeginUpdate();
+
+            foreach (FileInfo fileInfo in filesArray)
+            {
+                listViewItem = new ListViewItem(fileInfo.Name);
+                listView1.Items.Add(listViewItem);
+            }
+
+            listView1.EndUpdate();
+        }
+
+        public void AddDirs(TreeNode treeNode)
+        {
+            string fullPath = treeNode.FullPath;
+            DirectoryInfo directoryPath = new DirectoryInfo(fullPath);
+            DirectoryInfo[] directoryArray = { };
+
+            try
+            {
+                if (directoryPath.Exists)
+                {
+                    directoryArray = directoryPath.GetDirectories();
+                }
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+
+            foreach (DirectoryInfo directory in directoryArray)
+            {
+                TreeNode treeNodeDir = new TreeNode(directory.Name);
+                treeNode.Nodes.Add(treeNodeDir);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowDrives();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ShowFileNames();
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView1.BeginUpdate();
+
+            foreach (TreeNode treeNodeItem in e.Node.Nodes)
+            {
+                AddDirs(treeNodeItem);
+            }
+
+            treeView1.EndUpdate();
+        }
     }
 }
+
+/*Todo:
+ 1. Exception handling
+ 
+ */
