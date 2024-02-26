dd08b41246a89b964a0563faaf56f87f8292a84d
Admin <Admin@KIHWIN>
2024-02-26 11:55:24 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DiskBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            treeView1 = new TreeView();
+            listView1 = new ListView();
+            splitter1 = new Splitter();
+            columnHeader1 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // treeView1
+            // 
+            treeView1.Dock = DockStyle.Left;
+            treeView1.Location = new Point(0, 0);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(251, 450);
+            treeView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1 });
+            listView1.Dock = DockStyle.Fill;
+            listView1.Location = new Point(251, 0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(549, 450);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // splitter1
+            // 
+            splitter1.Location = new Point(251, 0);
+            splitter1.Name = "splitter1";
+            splitter1.Size = new Size(3, 450);
+            splitter1.TabIndex = 2;
+            splitter1.TabStop = false;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Filename";
+            columnHeader1.Width = 200;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitter1);
+            Controls.Add(listView1);
+            Controls.Add(treeView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TreeView treeView1;
+        private ListView listView1;
+        private Splitter splitter1;
+        private ColumnHeader columnHeader1;
+    }
+}
